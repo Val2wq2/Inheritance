cfb7ecd92994c334462227f919da881f2335644f
Tosin <AKINMOLADUNO0214@students.bowie.edu>
2026-09-17 12:29:13 -0400

    Inheritance-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// https://github.com/Val2wq2/Inheritance
 public class Main {
     public static void main(String[] args) {
         // Create two Person object
@@ -11,5 +10,10 @@ public class Main {
         System.out.println(p2.age);
 
         Person p3 = p2.Call_Private();
+
+        Student s1 = new Student();
+        s1.age = 20;
+        s1.gpa = 4.00;
+        System.out.println(s1.gpa);
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,23 @@
+// Student class will inherit Person Class
+// How to inherit: extends
+// How many class can a class inherit: 1
+
+// Student is the child class and Person is the parent class
+// Student is the sub class and Person is the super class
+// Person is the base class
+public class Student extends Person {
+    // Attributes are inherited - Not the private ones
+    // Methods are inherited - Not the private methods
+    // Setters and Getters are inherited - Not the private methods
+
+    // Constructors - Are not inherited, but you can use the constructors
+
+    int student_id;
+    double gpa;
+    Student(){
+
+    }
+
+
+}
+
