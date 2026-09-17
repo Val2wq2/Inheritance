58ef41e46e64884290c82246b9b0dbf92172c9db
Tosin <AKINMOLADUNO0214@students.bowie.edu>
2026-09-17 10:23:11 -0400

    Inheritance-2

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,4 @@
+// Private and protected are not allowed as class access modifiers
 public class Person {
     // height, age, color, name, weight
     double height;
@@ -6,6 +7,7 @@ public class Person {
     String name;
     double weight;
 
+    // Constructors can have private, protected, public access modifiers
     Person(){
         // Default constructor can be empty
         height = 48;
@@ -15,13 +17,28 @@ public class Person {
         weight = 140;
     }
 
+    // Protected members are accessible within the same package - java files under the same directory/folder
+    protected Person(double height,int age){
+        // Parameters are local variables to the constructor
+        this.height = height;
+        this.age = age;
+    }
+
     // Overloaded constructor
-    Person(double height,int age, String color, String name, double weight){
+    private Person(double height,int age, String color, String name, double weight){
         // Parameters are local variables to the constructor
         this.height = height;
         this.age = age;
         this.color = color;
         this.name = name;
         this.weight = weight;
+    }
+
+
+    // Setter and Getter
+    // Methods
+    public Person Call_Private(){
+        Person p3 = new Person(72, 70, "Black","Barack Obama",170);
+            return p3;
     }
 }
